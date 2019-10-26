14aec913665254efcb19603386a45b0dbe789a52
arturgspb <art@realweb.ru>
2019-10-26 15:16:53 +0300

    init

--- garpun/auth/check_token.py
+++ garpun/auth/check_token.py
@@ -1,5 +1,5 @@
 import httplib2
-from googleapiclient.discovery import build
+import requests
 from oauth2client.client import Credentials
 from oauth2client.file import Storage
 
@@ -12,5 +12,20 @@ print(u"cred.to_json() = %s" % str(credentials.to_json()))
 http = credentials.authorize(httplib2.Http())
 credentials.refresh(http)
 
-print(u"credentials.access_token = %s" % str(credentials.access_token))
-# youtube = build("qweqwe", "v3", http=authorize)
+print(u"credentials.access_token = %s" % str())
+
+configuration = {
+    "download": {
+        "dbQuery": {
+            "command": """
+            SELECT COALESCE(NULLIF(organization_id, 2), 42) as org, SUM(salary) as sum_salary
+            FROM meta_samples.employee
+            GROUP BY org
+            ORDER BY sum_salary DESC
+            """
+        }
+    }
+}
+resp = requests.post("http://apimeta.devision.io/api/v1/meta/metaql/download-data?access_token=" + credentials.access_token, json=configuration)
+
+print(u"resp.text = %s" % str(resp.text))
